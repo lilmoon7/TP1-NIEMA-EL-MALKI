a93440ba5e96e66a137910917eab27e7193cd165
Niema <216938596+lilmoon7@users.noreply.github.com>
2026-04-06 17:49:39 +0100

    Remove Personne class from Etudiant.java
    Removed the Personne class and its methods from Etudiant.java.

--- Etudiant.java
+++ Etudiant.java
@@ -11,20 +11,3 @@ class Etudiant extends Personne {
     }
 }
 
-class Personne {
-    private String nom;
-    private String prenom;
-
-    public Personne(String nom, String prenom) {
-        this.nom = nom;
-        this.prenom = prenom;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public String getPrenom() {
-        return prenom;
-    }
-}
