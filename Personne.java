760cc3f14eba5dd9684dd8433ec6b3cbef60dfb0
Niema <216938596+lilmoon7@users.noreply.github.com>
2026-04-06 17:33:18 +0100

    Add Personne class with name and surname fields

---
+++ Personne.java
@@ -0,0 +1,17 @@
+class Personne {
+    private String nom;
+    private String prenom;
+
+    public Personne(String nom, String prenom) {
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+}// Classe Etudiant pour le TP1
